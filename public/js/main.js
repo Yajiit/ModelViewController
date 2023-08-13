03cd1f0ecbd0d2f5e4a70cdbb1d4edf3d71b5aa8
Yajiit <tjagerter@gmail.com>
2023-08-13 01:28:36 -0400

    added homepage button to nav bar.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 // main.js
 document.getElementById('dashboard').addEventListener('click', goToDashboard);
+document.getElementById('homepage').addEventListener('click', goToHomepage);
+
 
 function goToDashboard() {
   window.location.href = '/dashboard';
+}
+
+function goToHomepage() {
+  window.location.href = '/';
 }
