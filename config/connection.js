7341499612e1e901c877d8e0c9de0fcc4c4964c2
Yajiit <tjagerter@gmail.com>
2023-08-13 16:33:36 -0400

    edited connections for heroku deployment.

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 // S E Q U E L I Z E
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
+  process.env.CLEARDB_DATABASE_URL || process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
+    host: process.env.DB_HOST || 'localhost',
     dialect: 'mysql',
-    port: 3306,
+    port: process.env.DB_PORT || 3306,
   }
 );
 
