82aed3d600ec01bf1fc532ab4177054b31edbb3c
Yajiit <tjagerter@gmail.com>
2023-08-13 16:54:02 -0400

    updating configurations for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
+let sequelize;
 // S E Q U E L I Z E
-const sequelize = new Sequelize(
-  process.env.CLEARDB_DATABASE_URL || process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-  }
-);
+if (process.env.CLEARDB_DATABASE_URL) {
+  // Use ClearDB URL if available (Heroku)
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  // Use local environment variables for development
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST || 'localhost',
+      dialect: 'mysql',
+      port: process.env.DB_PORT || 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
